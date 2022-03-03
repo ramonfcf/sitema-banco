1710720b903821e2a7eda880dadb34ea25926bbd
ramonfcf <ramonfcf@gmail.com>
2022-03-03 09:05:10 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,45 @@
+class Cliente {
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    #saldo = 1000;
+
+    depositar(valor){
+        if(valor > 0) {
+            this.#saldo += valor;
+            console.log(`Seu saldo atualizado é de R$${this.#saldo}`);
+        }
+    }
+
+    sacar(valor){
+        if (this.#saldo >= valor){
+            this.#saldo -= valor;
+        } else {
+            console.log('Você não possui saldo suficiente')
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 12345678910;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 11223344556;
+
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.agencia = 1001;
+
+//ContaCorrenteRicardo.sacar(200);
+ContaCorrenteRicardo.depositar(150);
+ContaCorrenteRicardo.depositar(50);
+
+
+
+console.log(ContaCorrenteRicardo);
+
+
